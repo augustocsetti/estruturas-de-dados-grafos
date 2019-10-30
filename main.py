29f8e6c022d6bb889eae90d376b0ef9c4e627dc1
augustocsetti <38505321+augustocsetti@users.noreply.github.com>
2019-10-30 11:23:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-#main
+#grafo
+
+class Graph():
+
+    def new(value):
+        self.node = Node(value)
+
+    def push():
+
+    def pop():
+
+    
+
+
+class Node(value):
+        def __init__(self): # initialize graph
+
+            self.value = value
+            self.next = False
+
+
+
+def main():
+    novo = new(5)
+
+
+main()
