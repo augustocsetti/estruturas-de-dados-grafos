445e6f7f95ec41f4bbb7510cbec29a328210f857
MuriloVS <40902924+MuriloVS@users.noreply.github.com>
2019-11-02 20:20:41 -0300

    Entrada e código alterados
    Alterações feitas no arquivo de entrada: primeira linha são os nodos, as linhas seguintes as arestas.
    
    Código: removida a classe Node - agora quando criamos o grafo já passamos os nodos e arestas por parâmetro. Os métodos que mostram a lista e o grau dos nodos foram implementadas. A que mostra lista precisa ser melhorada.

--- main.py
+++ main.py
@@ -4,14 +4,12 @@
     Murilo
 '''
 
-
 class Graph():  # classe para o grafo e seus métodos
 
-    # método que cria um novo nodo
-    # recebe por parâmetro o valor do nodo(sua chave/identicação)
-    def new(self, value):
-        self.node = Node(value)
-
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
+        self.edges = edges                       
+ 
     # método para inserir um novo nodo a um grafo já existente
     # recebe sua identificação e a quais nodos se liga (seus vértices)
     def push(self, value, vertex):
@@ -25,40 +23,57 @@ class Graph():  # classe para o grafo e seus métodos
 
     def remove(self):
         pass
-
+    
+    # mostra lista com os nodos e suas arestas
     def view(self):
-        pass
+        for i in range(len(self.nodes)):            
+            print(f'{self.nodes[i]}: ', end='')            
+            for j in range(len(self.edges)):
+                if self.nodes[i] in self.edges[j]:
+                    print(f'{self.edges[j]} ', end='')
+            print()
 
     def identify(self):
         pass
 
-    def grade(self):
-        pass
+    # mostra o grau do nodo
+    def grade(self, node):
+        self.count = 0
+        for edge in self.edges:
+            if str(node) in list(edge):
+                self.count += 1
+        print(f'Grau do nodo {node} = {self.count}')
+        
+
+# função para receber entrada - sugestão: mover para main
+def readFile():  
+    with open('entrada.txt') as file:        
+        lines = [line.rstrip() for line in file]  
+
+    return lines
 
 
-class Node():  # classe para cada nodo
+def main():    
+    
+    # lê os dados do arquivo de entrada
+    data = readFile()
 
-    def __init__(self, value):
-        self.value = value
-        self.next = False
+    # cria uma lista com os nodos a partir da primeira linha lida do arquivo de entrada
+    nodes = []
+    for char in data[0]:
+        if char != ',':
+            nodes.append(char)
 
+    # as linhas seguintes são as arestas
+    edges = data[1:]
+    
+    # cria o objeto passando como parâmetro os nodos e arestas
+    g = Graph(nodes, edges)    
 
-def readFile():  # função para receber entrada - sugestão: mover para main
-    with open('entrada.txt') as arquivo:
-        linhas = [linha.rstrip() for linha in arquivo]  # remove tudo a direita - \n
-
-    return linhas
-
-
-def main():
-
-    entrada = readFile()
-    print(f'Entrada = {entrada}')
-
-    g = []
-    g.append(Graph())
-
-    print(g)
-
+    # testes
+    g.view()
+    for node in nodes:
+        g.grade(node)
+  
 
 main()
