3d6fd42a9b07cef07f7b05beb9bf80de2b3d0038
MuriloVS <40902924+MuriloVS@users.noreply.github.com>
2019-11-03 11:12:47 -0300

    Arquivo de entrada e Matriz de Adjacências
    - feita nova moudança no arquivo de entrada para facilitar a manipulação das strings;
    
    - função que mostra a matriz de adjacências implementada.

--- main.py
+++ main.py
@@ -43,6 +43,28 @@ class Graph():  # classe para o grafo e seus métodos
             if str(node) in list(edge):
                 self.count += 1
         print(f'Grau do nodo {node} = {self.count}')
+
+    # mostra a matriz de adjacência do grafo
+    def adjacencyMatrix(self):
+        # print os nodos na horizontal - primeira linha
+        print('  ', end='')
+        for i in range(len(self.nodes)):
+            print(f'{self.nodes[i]} ', end='')
+        
+        print()
+
+        # procura pelas nodos que possuem arestas entre si
+        for i in range(len(self.nodes)):
+            print(f'{self.nodes[i]} ', end='')
+                        
+            for j in range(len(self.nodes)):
+                # a comparação abaixo é feita com or para garantir que
+                #  tanto (1, 5) quanto (5, 1) sejam registdas correntamente
+                if list([self.nodes[i], self.nodes[j]]) in self.edges or list([self.nodes[j], self.nodes[i]]) in self.edges:
+                    print('1 ', end='')
+                else:
+                    print('0 ', end='')
+            print()
         
 
 # função para receber entrada - sugestão: mover para main
@@ -61,11 +83,14 @@ def main():
     # cria uma lista com os nodos a partir da primeira linha lida do arquivo de entrada
     nodes = []
     for char in data[0]:
-        if char != ',':
-            nodes.append(char)
+        nodes.append(char)
 
     # as linhas seguintes são as arestas
-    edges = data[1:]
+    #edges = data[1:]
+    edges = []
+    for linha in data[1:]:
+        edges.append([linha[0], linha[1]])
+    
     
     # cria o objeto passando como parâmetro os nodos e arestas
     g = Graph(nodes, edges)    
@@ -74,6 +99,6 @@ def main():
     g.view()
     for node in nodes:
         g.grade(node)
-  
+    g.adjacencyMatrix()
 
 main()
