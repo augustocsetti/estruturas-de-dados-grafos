5f1b7bdd2ea3b6f7fc7f81b132a2cd11b06a9bbc
MuriloVS <40902924+MuriloVS@users.noreply.github.com>
2019-11-04 21:14:41 -0300

    Pop, remove
    Feita a implementação do método para remover arestas o que implicou em alterações no método pop - agora quando um nodo é excluído o método remove é chamado automaticamente para remover as arestas que faziam ligação com este nodo. Parece estar ok.

--- main.py
+++ main.py
@@ -4,35 +4,62 @@
     Murilo
 '''
 
+
 class Graph():  # classe para o grafo e seus métodos
 
     def __init__(self, nodes, edges):
         self.nodes = nodes
-        self.edges = edges                       
- 
-    # método para inserir um novo nodo a um grafo já existente   
+        self.edges = edges
+
+    # método para inserir um novo nodo a um grafo já existente
     def push(self, node):
         self.nodes.append(node)
 
     # método que remove um nodo
     def pop(self, node):
-        for i in range(len(self.nodes)):        
-            if node == self.nodes[i]:                            
-                self.nodes.pop(i)
+        self.aux = []
+        self.index = -1
+
+        # procura o íncide no nodo a ser excluído
+        for i in range(len(self.nodes)):
+            if node == self.nodes[i]:
+                self.index = i
                 break
-    
+
+        if self.index >= 0:
+            # não confundir com o método pop implementado - abaixo é o de listas
+            self.nodes.pop(self.index)
+
+            # procura por arestas que contenham o nodo excluído
+            for i in range(len(self.edges)):
+                if node in ''.join(self.edges[i]):
+                    self.aux.append(''.join(self.edges[i]))
+
+            # chama o método para remover arestas
+            for i in range(len(self.aux)):
+                self.remove(self.aux[i])
+
     # método para inserir arestras entre dois nodos já existentes
-    def insert(self, edge):        
+    def insert(self, edge):
         self.edges.append([edge[0], edge[1]])
 
     # método para remover arestas
     def remove(self, edge):
-        pass
-    
+        # esta variável guarda as arestas que não serão excluídas
+        # ela foi criada para evitar problemas de índice
+        # eles ocorriam quanto o pop era usado no meio do loop
+        self.temp = []
+
+        for i in range(len(self.edges)):
+            if [edge[0], edge[1]] != self.edges[i]:
+                self.temp.append(self.edges[i])
+
+        self.edges = self.temp
+
     # mostra lista com os nodos e suas arestas
     def view(self):
-        for i in range(len(self.nodes)):            
-            print(f'{self.nodes[i]}: ', end='')            
+        for i in range(len(self.nodes)):
+            print(f'{self.nodes[i]}: ', end='')
             for j in range(len(self.edges)):
                 if self.nodes[i] in self.edges[j]:
                     print(f'{self.edges[j]} ', end='')
@@ -55,13 +82,13 @@ class Graph():  # classe para o grafo e seus métodos
         print('  ', end='')
         for i in range(len(self.nodes)):
             print(f'{self.nodes[i]} ', end='')
-        
+
         print()
 
         # procura pelas nodos que possuem arestas entre si
         for i in range(len(self.nodes)):
             print(f'{self.nodes[i]} ', end='')
-                        
+
             for j in range(len(self.nodes)):
                 # a comparação abaixo é feita com 'or' para garantir que
                 #  tanto (1, 5) quanto (5, 1) sejam registradas correntamente
@@ -70,18 +97,18 @@ class Graph():  # classe para o grafo e seus métodos
                 else:
                     print('0 ', end='')
             print()
-        
+
 
 # função para receber entrada - sugestão: mover para main
-def readFile():  
-    with open('entrada.txt') as file:        
-        lines = [line.rstrip() for line in file]  
+def readFile():
+    with open('entrada.txt') as file:
+        lines = [line.rstrip() for line in file]
 
     return lines
 
 
-def main():    
-    
+def main():
+
     # lê os dados do arquivo de entrada
     data = readFile()
 
@@ -91,31 +118,32 @@ def main():
         nodes.append(char)
 
     # as linhas seguintes são as arestas
-    #edges = data[1:]
     edges = []
     for linha in data[1:]:
         edges.append([linha[0], linha[1]])
-    
-    
+
     # cria o objeto passando como parâmetro os nodos e arestas
-    g = Graph(nodes, edges)    
+    g = Graph(nodes, edges)
 
     # testes
     g.view()
     for node in nodes:
         g.grade(node)
-    g.adjacencyMatrix()
+    # g.adjacencyMatrix()
     g.push('6')
-    g.adjacencyMatrix()
+    # g.adjacencyMatrix()
     g.insert('62')
     g.insert('66')
+    # g.view()
+    # g.adjacencyMatrix()
+    g.pop('2')
     g.view()
-    g.adjacencyMatrix()
-    g.pop('3')
-    g.view()
-    g.adjacencyMatrix()
-    g.pop('1')
-    g.view()
-    g.adjacencyMatrix()
+    # g.adjacencyMatrix()
+    # g.pop('1')
+    # g.view()
+    # g.adjacencyMatrix()
+    # g.remove('12')
+    # g.view()
+
 
 main()
