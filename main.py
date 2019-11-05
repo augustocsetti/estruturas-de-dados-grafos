a809d295552f3ac446140b630511ed69a29639e7
MuriloVS <40902924+MuriloVS@users.noreply.github.com>
2019-11-05 14:34:12 -0300

    Melhoras no código e na classe grafo
    - preparação para trabalhar com grafos direcionados;
    
    - 'esqueleto' do menu;
    
    - grafos são guardados em uma lista agora (necessário para quando o usuário quiser trabalhar com outro grafo).

--- main.py
+++ main.py
@@ -7,9 +7,10 @@
 
 class Graph():  # classe para o grafo e seus métodos
 
-    def __init__(self, nodes, edges):
+    def __init__(self, nodes='', edges='', directed=False):
         self.nodes = nodes
         self.edges = edges
+        self.directed = directed
 
     # método para inserir um novo nodo a um grafo já existente
     def push(self, node):
@@ -20,7 +21,7 @@ class Graph():  # classe para o grafo e seus métodos
         self.aux = []
         self.index = -1
 
-        # procura o íncide no nodo a ser excluído
+        # procura o índice no nodo a ser excluído
         for i in range(len(self.nodes)):
             if node == self.nodes[i]:
                 self.index = i
@@ -71,14 +72,16 @@ class Graph():  # classe para o grafo e seus métodos
     # mostra o grau do nodo
     def grade(self, node):
         self.count = 0
+
         for edge in self.edges:
             if str(node) in list(edge):
                 self.count += 1
+
         print(f'Grau do nodo {node} = {self.count}')
 
     # mostra a matriz de adjacência do grafo
     def adjacencyMatrix(self):
-        # print os nodos na horizontal - primeira linha
+        # mostra os nodos na horizontal - primeira linha
         print('  ', end='')
         for i in range(len(self.nodes)):
             print(f'{self.nodes[i]} ', end='')
@@ -91,20 +94,33 @@ class Graph():  # classe para o grafo e seus métodos
 
             for j in range(len(self.nodes)):
                 # a comparação abaixo é feita com 'or' para garantir que
-                #  tanto (1, 5) quanto (5, 1) sejam registradas correntamente
-                if list([self.nodes[i], self.nodes[j]]) in self.edges or list([self.nodes[j], self.nodes[i]]) in self.edges:
+                #  tanto (1, 5) quanto (5, 1) sejam registradas corretamente (não direcionado)
+                if [self.nodes[i], self.nodes[j]] in self.edges or [self.nodes[j], self.nodes[i]] in self.edges:
                     print('1 ', end='')
                 else:
                     print('0 ', end='')
             print()
 
 
-# função para receber entrada - sugestão: mover para main
+# função para receber entrada
 def readFile():
     with open('entrada.txt') as file:
         lines = [line.rstrip() for line in file]
 
     return lines
+
+# menu do programa
+def menu():
+    print('===============Opções===============')
+    print('1 - Mostrar lista de adjacências')
+    print('2 - Mostrar matriz de adjacências')
+    print('3 - Inserir um nodo')
+    print('4 - Remover um nodo')
+    print('5 - Inserir uma aresta')
+    print('6 - Remover uma aresta')
+    print('7 - Criar um novo grafo')
+    print('====================================')
+    option = input('Opção: ')
 
 
 def main():
@@ -123,27 +139,34 @@ def main():
         edges.append([linha[0], linha[1]])
 
     # cria o objeto passando como parâmetro os nodos e arestas
-    g = Graph(nodes, edges)
+    grafos = []
+    grafos.append(Graph(nodes, edges))
 
     # testes
-    g.view()
+    grafos[0].view()
     for node in nodes:
-        g.grade(node)
-    # g.adjacencyMatrix()
-    g.push('6')
-    # g.adjacencyMatrix()
-    g.insert('62')
-    g.insert('66')
-    # g.view()
-    # g.adjacencyMatrix()
-    g.pop('2')
-    g.view()
-    # g.adjacencyMatrix()
-    # g.pop('1')
-    # g.view()
-    # g.adjacencyMatrix()
-    # g.remove('12')
-    # g.view()
+        grafos[0].grade(node)
+    # grafos[0].adjacencyMatrix()
+    grafos[0].push('6')
+    grafos[0].adjacencyMatrix()
+    grafos[0].insert('62')
+    grafos[0].insert('66')
+    # grafos[0].view()
+    # grafos[0].adjacencyMatrix()
+    grafos[0].pop('2')
+    grafos[0].view()
+    # grafos[0].adjacencyMatrix()
+    # grafos[0].pop('1')
+    # grafos[0].view()
+    grafos[0].adjacencyMatrix()
+    grafos[0].remove('15')
+    grafos[0].view()
+    print('------')
+    grafos.append(Graph('', '', True))
+    grafos[1].view()
+    grafos[1].adjacencyMatrix()
+    print(grafos[1].directed)
+    menu()
 
 
 main()
